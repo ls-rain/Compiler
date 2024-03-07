33799f6fbbdb640fa0af9cedca75fc01d3e64f78
ls-rain <127829709+ls-rain@users.noreply.github.com>
2024-03-07 22:04:03 +0900

    Add files via upload
    테스트 파일들

---
+++ SyntaxAnalysis2/test/test6.c
@@ -0,0 +1,17 @@
+int main(int argc, char *argv[])
+{
+	int kim = 3;
+	switch(kim) {
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		default:
+			break;
+	}
+	return 0;
+}
